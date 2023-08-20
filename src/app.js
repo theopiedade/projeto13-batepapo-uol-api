22daeaa735596db241ba4136becbad4f28f9d01f
Theo <theo.piedade@gmail.com>
2023-08-20 14:14:55 -0300

    Joi Schemas made

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@ import express from "express";
 import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import joi from 'joi';
 
 const app = express();
 app.use(express.json());
 app.use(cors);
 dotenv.config();
+
+const userSchema = joi.object({
+    name: joi.string().required(),
+    laststatus: joi.date().required()
+});
+
+const msgSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required(),
+    time: joi.string().required()
+});
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
@@ -16,5 +30,7 @@ mongoClient.connect()
  .catch((err) => console.log(err.message));
 
 
-const PORT = 5005
+
+
+const PORT = 5000
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
