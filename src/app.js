be79fd63aac6bad070294ab7b69dc5422485917e
Theo <theo.piedade@gmail.com>
2023-08-20 21:07:56 -0300

    app.post complete

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import cors from "cors"
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(express.json());
 app.use(cors);
 dotenv.config();
+const dayjs = require('dayjs');
+dayjs().format();
+
 
 const nameSchema = joi.object({
     name: joi.string().required()
@@ -47,8 +51,19 @@ mongoClient.connect()
             name: name,
             lastStatus: Date.now()
         }
-
         await db.collection('participants').insertOne(user);
+        
+        //let time = dayjs().get('hour')+':'+dayjs().get('minute')+':'+dayjs().get('second');
+        let time = dayjs(Date.now()).format('HH:mm:ss');
+
+        const msg = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: time
+        }
+        await db.collection('messages').insertOne(msg);
   
       res.sendStatus(201);
     } catch (err) {
